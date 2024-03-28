40a49ddce1e267662615a74fb52929432b38eda9
itx-tirox <165162801+itx-tirox@users.noreply.github.com>
2024-03-29 01:02:00 +0530

    Update gpt.py

--- DAXXMUSIC/plugins/tools/gpt.py
+++ DAXXMUSIC/plugins/tools/gpt.py
@@ -1,16 +1,16 @@
 import requests
-from DAXXMUSIC import app as Mukesh
+from DAXXMUSIC import app
 import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 from config import BOT_NAME, BOT_USERNAME
 
-@Mukesh.on_message(filters.command(["lol"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+@app.on_message(filters.command(["lol"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 async def chat_gpt(app, message):
     
     try:
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
             "Example:**\n\n`/ask What is Chat Gpt?`")
