f62850555094513c28cc043452ba2fd06b32bbd8
itx-tirox <165162801+itx-tirox@users.noreply.github.com>
2024-03-29 00:46:44 +0530

    Update gpt.py

--- DAXXMUSIC/plugins/tools/gpt.py
+++ DAXXMUSIC/plugins/tools/gpt.py
@@ -4,7 +4,7 @@ import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
-from config import MUSIC_BOT_NAME
+from config import BOT_NAME, BOT_USERNAME
 
 @Mukesh.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 async def chat_gpt(bot, message):
@@ -13,10 +13,10 @@ async def chat_gpt(bot, message):
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n`/chatgpt Where is TajMahal?`")
+            "Example:**\n\n`/ask What is Chat Gpt?`")
         else:
             a = message.text.split(' ', 1)[1]
             r=api.chatgpt(a,mode="elonmusk")["results"]
-            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @AnnieMusicBot ", parse_mode=ParseMode.MARKDOWN)     
+            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
