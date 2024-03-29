71d4453c0a23f61749d650125b79a455be269091
itx-tirox <165162801+itx-tirox@users.noreply.github.com>
2024-03-29 12:30:05 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -12,14 +12,14 @@ async def ai_bot(_, message):
       gonb = Ai(query=queri)
       await message.reply(gonb.chat())
      elif len(message.command) == 1:
-      return await message.reply("ʜᴇʟʟᴏ! ɪ'ᴍ ʏᴏᴜʀ ғʀɪᴇɴᴅʟʏ ᴀɪ ᴀssɪsᴛᴀɴᴛ. \nʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏᴅᴀʏ?")
+      return await message.reply("ʜᴇʟʟᴏ 🥀\nɪ'ᴍ ᴀ ᴀɪ ᴀssɪsᴛᴀɴᴛ Fᴏʀ Aɴsᴡᴇʀ Yᴏᴜʀ Aɴʏ ǫᴜᴇsᴛɪᴏɴs, ʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ \n\nᴊᴏɪɴ [Oᴜʀ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ](https://t.me/BikashGadgetsTech)")
      elif len(message.command) > 1:
       queri = message.text.split(None,1)[1]
      gonb = Ai(query=queri)
      x = Ai(query=queri)
      me = await message.reply_text("ᴘʀᴏᴄᴇssᴇs.....")
      await rest(2)
-     mee = await me.edit_text("ᴀʟʟ ᴍᴏsᴛ ᴅᴏɴᴇ ʙᴀʙʏ....🖤")
+     mee = await me.edit_text("ᴀʟʟ ᴍᴏsᴛ ᴅᴏɴᴇ ....")
      await mee.delete()
      await rest(1)
      await message.reply(gonb.chat())
